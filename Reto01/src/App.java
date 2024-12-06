d0285811897ee5ad5991925dd8c01e1b8f930f54
William Amaya Castaño PcTp <williamjavi.140@gmail.com>
2024-12-06 10:59:03 -0500

    Eliminacion Reto

--- Reto01/src/App.java
+++
@@ -1,8 +0,0 @@
-
-import simuladorInterplanetario.SimuladorInterplanetario;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        SimuladorInterplanetario.startGame();
-    }
-}
