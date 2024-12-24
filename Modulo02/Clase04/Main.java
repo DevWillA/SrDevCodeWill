dd0d79a724de835e663a64111a0787b37c82c41d
William Amaya Castaño <williamjavi.140@gmail.com>
2024-12-23 19:55:33 -0500

    Actualizacon clase 04 modulo02

--- Modulo02/Clase04/Main.java
+++ Modulo02/Clase04/Main.java
@@ -1,10 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-
         Logger logger1 = Logger.getInstancia();
         Logger logger2 = Logger.getInstancia();
 
+
+        logger1.log("Se Imprimio el pasaporte");
+        logger2.log("Se Imprimio la historia clinica");
     }
+
 
 }
