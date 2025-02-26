2cabe9a7535e97037a1d4443422436b6becf7df7
WillDevLenovo <williamjavi.140@gmail.com>
2025-02-26 18:43:35 -0500

    Upgrade clase01

--- Modulo03/Clase01/App.java
+++ Modulo03/Clase01/App.java
@@ -1,32 +1,37 @@
-public package Modulo03.Clase01;
+// Version: 1.0
+// Date: 2021-07-07
+
+package Modulo03.Clase01;
 
 import java.util.Scanner;
 
 class App {
 
     public static void main(String[] args) {
-            var sc = new Scanner(System.in);
+        var sc = new Scanner(System.in);
 
-    System.out.println("Bienvenido a la aplicacion, ingrese una cadena y la pasare a mayusculas");
-    var app = new App();
-    var cadena = app.readFromKeyboard(sc);
-    System.out.printf("la cadena en mayusculas es: '%s'%n", cadena.toUpperCase());
+        System.out.println("Bienvenido a la aplicacion, ingrese una cadena y la pasare a mayusculas");
+        var app = new App();
 
-    sc.close();
+        try {
+            var cadena = app.readFromKeyboard(sc);
+            System.out.printf("la cadena en mayusculas es: '%s'%n", cadena.toUpperCase());
+        } catch (BlankStringException e) {
+            System.out.println("Acaba de ocurrir un error " + e.getMessage());
+            e.printStackTrace(System.err); // muestra el error
+        }
+        sc.close();
 
     }
 
-    private String readFromKeyboard(Scanner sc)  throws BlankStringException {
+    private String readFromKeyboard(Scanner sc) throws BlankStringException {
         System.out.println("Ingrese una cadena");
         var cadena = sc.nextLine();
         if (cadena.isBlank()) {
             throw new BlankStringException("La cadena no puede ser vacia");
         }
 
-        
-        return sc.nextLine();
+        return cadena.trim();
 
-
-        
     }
 }
