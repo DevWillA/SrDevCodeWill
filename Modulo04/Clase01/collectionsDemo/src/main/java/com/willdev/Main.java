9286137752ed3038afdb785863d1c90afdc8c048
WillDevLenovo <williamjavi.140@gmail.com>
2025-03-26 18:45:57 -0500

    Clase 01 finish

--- Modulo04/Clase01/collectionsDemo/src/main/java/com/willdev/Main.java
+++ Modulo04/Clase01/collectionsDemo/src/main/java/com/willdev/Main.java
@@ -2,6 +2,6 @@ package com.willdev.collectionsdemo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var test = new List();
     }
 }
--- Modulo04/Clase01/collectionsDemo/src/main/java/com/willdev/collections/Lists.java
+++ Modulo04/Clase01/collectionsDemo/src/main/java/com/willdev/collections/Lists.java
@@ -1,11 +1,37 @@
 package com.willdev.collections;
 
+import java.util.LinkedList;
+import java.utils.*;
+
 public class Lists {
 
     public void performDemo() {
 
+        List list = new LinkedList();
+        list.add(1);
+        list.add(10.0);
+        list.add("Will");
+        list.add(1,"casa");
+
+        Object dato  = list.get(0);
+        Integer entero = (Integer) dato;
+
+        System.out.println(entero);
+
+
+        for (int i =  0;i<list.size();i++)
+{
+    System.out.println(list.get(i));
+}
+
+        for (var element : list){
+            System.out.println(element);
+        }
+
+        var list 2 = new ArrayList();
+
+
+
     }
-
-
 
 }
